3fa16e2c78cc31157550a23914853a1829126364
dom <d.bladek@gmail.com>
2017-02-12 23:54:13 +0000

    Chapter 7 complete

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,7 +3,8 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    MAX_LEN = 128
+    name = models.CharField(max_length=MAX_LEN, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from rango.models import Category,Page
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, PageForm
 
 
 
 # Create your views here.
 
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
 
 
 def add_category(request):
