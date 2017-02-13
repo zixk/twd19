a494f8bb3d4fac831a357b001dfa65be01332a75
dom <d.bladek@gmail.com>
2017-02-13 14:20:35 +0000

    Chapter 10.5 complete

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,10 +5,25 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 # Create your views here.
 
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits','1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+
+        response.ser_cookie('last_visit', str(datetime.now()))
+    else:
+        visits = 1
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
 
 @login_required
 def user_logout(request):
@@ -123,13 +138,20 @@ def show_category(request, category_name_slug):
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
     context_dict1 = {'boldmessage': "Dominik"}
     return render(request, 'rango/about.html', context=context_dict1)
 
 def index(request):
+
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list,
                     'pages': page_list}
+    response = render(request, 'rango/index.html', context_dict)
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
+    return response
